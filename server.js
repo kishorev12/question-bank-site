db108b7fe91404d0ab46b4004dd3620a649b14fa
kishorev12 <42258279+kishorev12@users.noreply.github.com>
2025-05-08 18:55:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
@@ -14,4 +15,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/hostingusers', {
 
 app.use('/auth', authRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
