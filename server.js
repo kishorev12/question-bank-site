d4d735fedf67eca2e4ec299eec9cebf31f5dfe1f
kishorev12 <42258279+kishorev12@users.noreply.github.com>
2025-05-08 17:35:40 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/hostingusers', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(3000, () => console.log('Server running on port 3000'));
